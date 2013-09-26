b8658e878731c1924d86ec564ecccedb96302367
Christian Blanquera <cblanquera@gmail.com>
2013-09-26 17:04:04 +0800

    tg_PH to en_US

--- config/back/settings.php
+++ config/back/settings.php
@@ -2,7 +2,7 @@
 return array(
 	'url_root'			=> '',
 	'cdn_root'			=> '',
-	'i18n'				=> 'tg_PH',
+	'i18n'				=> 'en_US',
 	'eden_debug' 		=> true,
 	'debug_mode' 		=> E_ALL,
 	'server_timezone' 	=> 'Asia/Manila',
