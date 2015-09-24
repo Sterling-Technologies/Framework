9ef0e9a1dbca3c46bcbab6e4e621d8d5ad8d170c
Riri <namespace@ph1.enthropia.com>
2015-09-24 19:44:59 +0800

    default queue configuration

--- Api/settings/config.php
+++ Api/settings/config.php
@@ -3,4 +3,11 @@ return array(
 	'i18n'				=> 'en_US',
 	'debug_mode' 		=> E_ALL,
 	'server_timezone' 	=> 'Asia/Manila',
-	'default_action' 	=> 'index');
+	'default_action' 	=> 'index',
+	'queue' 			=> array(
+			'host' => 'localhost',
+			'port' => 5672,
+			'username' => 'guest',
+			'password' => 'guest'
+		)
+	);
